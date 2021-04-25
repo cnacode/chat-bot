442c04718de379cd7b66b232e4b4d358c91373cd
Sina Montazeri <sinamontazeri@outlook.com>
2021-04-26 02:26:43 +0430

    activate message service

--- src/loaders/services.ts
+++ src/loaders/services.ts
@@ -1,5 +1,7 @@
 import metaService from '../services/meta'
+import messageService from '../services/message'
 
 export default function (app: Application, deps: ServiceDependencies) {
     metaService(app, deps)
+    messageService(app, deps)
 }
