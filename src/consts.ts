888920d5a63de0eb3cbe569ce71fe9cdefc7e1e4
Sina Montazeri <sinamontazeri@outlook.com>
2021-04-26 02:25:27 +0430

    add expected answers

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const hello = ['button_hello', 'hi', 'hello', 'howdy', 'wassap', 'yo']
+export const yes = ['button_yes', 'yes', 'yep', 'yup', 'yeah', 'sure']
+export const no = ['button_no', 'no', 'nah', 'nope', `that's ok`, 'never']
