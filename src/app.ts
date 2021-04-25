49586f3151d1385ebe906d2c96e5eb5c6e67ac8b
Sina Montazeri <sinamontazeri@outlook.com>
2021-04-26 02:26:16 +0430

    set up application

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,47 @@
 import express from 'express'
-import loaders from './loaders'
 import config from './config'
+import createDatabase from './loaders/database'
+import createLogger from './loaders/logger'
+import expressMiddlewares from './loaders/express'
+import errorHandler from './loaders/errorHandler'
+import services from './loaders/services'
+import asyncHandler from './loaders/asyncHandler'
+import socketio from './loaders/socketio'
+import bot from './loaders/bot'
+import { createServer } from 'http'
 
 async function startServer() {
     const app = express()
+    const server = createServer(app)
     try {
-        await loaders(app)
+        // load logger
+        const logger = createLogger(app)
+        logger.info('✅logger loaded!')
+
+        //load express middlewares
+        expressMiddlewares(app)
+        logger.info('✅express loaded!')
+
+        //load errorHandler
+        errorHandler({ app, logger })
+        logger.info('✅error handler loaded!')
+
+        // establish database connection
+        const { database } = await createDatabase(logger)
+        if (!database) return logger.error(`database failed to load, exiting`)
+        logger.info('✅database models loaded!')
+
+        services(app, { logger, asyncHandler, database })
+
+        const { connect, disconnect } = socketio({ server, logger })
+
+        bot({ connect, disconnect, database })
+
+        server.listen(config.port)
+        logger.info(`ℹ️ application port ${config.port}`)
     } catch (e) {
         console.log(e)
     }
-    app.listen(config.port)
 }
 
 startServer()
